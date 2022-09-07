e61d40383ba22a5059675ac651135833679b0c5f
RaveenaBora <106060966+RaveenaBora@users.noreply.github.com>
2022-09-07 11:37:29 +0300

    Name and Age checker

---
+++ name and age checker.py
@@ -0,0 +1,5 @@
+age =int( input("please provide your age "))
+if age >= 18:
+    print("Welcome to the club")
+elif age < 18:
+    print ("Persons under the age of 18 are not allowed into this club")
